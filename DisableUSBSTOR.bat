b459d97d3cd8b64f05e4c815a8173c7268a78c05
Mazekaz <zanym1102@gmail.com>
2021-07-11 12:23:27 +0700

    Add comment

--- DisableUSBSTOR.bat
+++ DisableUSBSTOR.bat
@@ -1,4 +1,4 @@
-:: Run as Highest Privileges
+:: Run as Highest Privileges <> Configuration Type - COMPUTER
 :: Disable USB with Regedit
 
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR" /v Start /t REG_DWORD /d 4 /f
