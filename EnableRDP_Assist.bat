61d49f8ad8b8ee9dcbed97dd2738954fd26e71f5
Manhjisme <ZanyM1102@gmail.com>
2021-07-23 14:15:02 +0700

    Update EnableRDP_Assist.bat

--- EnableRDP_Assist.bat
+++ EnableRDP_Assist.bat
@@ -5,30 +5,12 @@
 :: 
 
 @echo off
+netsh firewall set service type=remotedesktop mode=enable
+netsh advfirewall firewall add rule name="Open Remote Desktop" protocol=TCP dir=in localport=3389 action=allow
+netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
 
-REM Double-Click File will Require Admin as Run as Administrator
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v AllowTSConnections  /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fAllowToGetHelp  /t REG_DWORD /d 1 /f
 
-if _%1_==_payload_  goto :payload
 
-:getadmin
-    echo %~nx0: elevating self
-    set vbs=%temp%\getadmin.vbs
-    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
-    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-goto :eof
-
-:payload
-
-::ENTER YOUR CODE BELOW::   
-
-  netsh firewall set service type=remotedesktop mode=enable
-  netsh advfirewall firewall add rule name="Open Remote Desktop" protocol=TCP dir=in localport=3389 action=allow
-  netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
-
-  reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v AllowTSConnections  /t REG_DWORD /d 0 /f
-  reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
-  reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fAllowToGetHelp  /t REG_DWORD /d 1 /f
-
-::END OF YOUR CODE::
